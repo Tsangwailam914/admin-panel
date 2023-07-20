38f335c02334271752f346f7e32de8ecd334def9
Wai Lam Tsang <wailamkelly.tsang@gmail.com>
2023-07-20 16:14:32 -0400

    added modal and product page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,12 +1,14 @@
-import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss"
 import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "avatar",
+    field: "img",
     headerName: "Avatar",
     width: 100,
     renderCell: (params) => {
@@ -28,19 +30,20 @@ const columns: GridColDef[] = [
   {
     field: "email",
     headerName: "Email",
-    width: 150,
-    editable: true,
+    width: 200,
+    type: 'string'
   },
   {
     field: "phone",
     headerName: "Phone",
-    width: 150,
-    editable: true,
+    width: 200,
+    type: "string"
   },
   {
     field: "createAt",
     headerName: "Created At",
-    width: 100,
+    width: 200,
+    type: 'string'
   },
   {
     field: "verified",
@@ -51,13 +54,16 @@ const columns: GridColDef[] = [
 ];
 
 const Users = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={() => {setOpen(true)}}>Add New User</button>
       </div>
       <DataTable slug='users' columns={columns} rows={userRows}/>
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   )
 };
